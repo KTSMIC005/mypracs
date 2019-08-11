7def9265ca2eaaaa75ccba01cc58aece466d3653
Katsie011 <48316327+Katsie011@users.noreply.github.com>
2019-08-11 23:34:18 +0200

    hash

--- Prac 2/Bash Compiler/BashRunO0.sh
+++ Prac 2/Bash Compiler/BashRunO0.sh
@@ -4,6 +4,16 @@ set -x
 
 make clean
 
+make 
+echo "fp16 unthreaded" >>partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+echo "" >> partO0.txt
+
+
 make threaded32
 echo "fp16 threaded32" >>partO0.txt
 make run_threaded32 >> partO0.txt
