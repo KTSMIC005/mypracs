cb6bbce9632cf2b143571f24b1d052222e8513d7
Katsie011 <48316327+Katsie011@users.noreply.github.com>
2019-08-11 20:10:22 +0200

    bash

--- Prac 2/BashRun.sh
+++ Prac 2/BashRun.sh
@@ -2,25 +2,48 @@
 
 set -x
 
-N = (1 2 3 4 5)
-
 make clean
 
 make
 
-for n in {0..5}
-do
-    echo "${N[n]} fp16 unthreaded" >> part1.txt
-    make run >> part1.txt
-    echo "" >>part12.txt
-done
+echo "fp16 unthreaded" >> partO1.txt
+make run >> partO1.txt
+echo "" >> partO1.txt
+
+make run >> partO1.txt
+echo "" >> partO1.txt
+
+make run >> partO1.txt
+echo "" >> partO1.txt
+
+make run >> partO1.txt
+echo "" >> partO1.txt
+
+make run >> partO1.txt
+echo "" >> partO1.txt
+
+echo "" >> partO1.txt
+
+
 
 make threaded
 
-for n in {0..5}
-do
+echo "fp16 threaded" >>partO1.txt
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
 
-    echo "${N[n]} fp16 threaded" >> part1.txt
-    make run_threaded >> part1.txt
-    echo "" >>part12.txt
-done    
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
+
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
+
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
+
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
+
+make run_threaded >> partO1.txt
+echo "" >> partO1.txt
+
