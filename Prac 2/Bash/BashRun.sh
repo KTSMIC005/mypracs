094d7c3cd6eb98c7d2ed914cc162cbba98d287c7
Katsie011 <48316327+Katsie011@users.noreply.github.com>
2019-08-11 21:24:39 +0200

    big edit

---
+++ Prac 2/Bash/BashRun.sh
@@ -0,0 +1,61 @@
+#!bin/bash
+
+set -x
+
+make clean
+
+make
+
+echo "fp16 unthreaded" >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+make run >> partO0.txt
+
+
+make threaded2
+
+echo "fp16 threaded2" >>partO0.txt
+make run_threaded2 >> partO0.txt
+make run_threaded2 >> partO0.txt
+make run_threaded2 >> partO0.txt
+make run_threaded2 >> partO0.txt
+make run_threaded2 >> partO0.txt
+
+
+make threaded4
+
+echo "fp16 threaded4" >>partO0.txt
+make run_threaded4 >> partO0.txt
+make run_threaded4 >> partO0.txt
+make run_threaded4 >> partO0.txt
+make run_threaded4 >> partO0.txt
+make run_threaded4 >> partO0.txt
+
+
+make threaded8
+echo "fp16 threaded8" >>partO0.txt
+make run_threaded8 >> partO0.txt
+make run_threaded8 >> partO0.txt
+make run_threaded8 >> partO0.txt
+make run_threaded8 >> partO0.txt
+make run_threaded8 >> partO0.txt
+
+
+make threaded16
+echo "fp16 threaded16" >>partO0.txt
+make run_threaded16 >> partO0.txt
+make run_threaded16 >> partO0.txt
+make run_threaded16 >> partO0.txt
+make run_threaded16 >> partO0.txt
+make run_threaded16 >> partO0.txt
+
+
+make threaded32
+echo "fp16 threaded32" >>partO0.txt
+make run_threaded32 >> partO0.txt
+make run_threaded32 >> partO0.txt
+make run_threaded32 >> partO0.txt
+make run_threaded32 >> partO0.txt
+make run_threaded16 >> partO0.txt
