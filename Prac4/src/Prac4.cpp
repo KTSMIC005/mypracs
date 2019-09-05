c8f79a9110ff7a7ee6c2630c98f14e638f3c10bf
Lawrence Stanton <52958935+LawrenceStanton@users.noreply.github.com>
2019-09-05 13:13:37 +0200

    Refactoring

--- Prac4/src/Prac4.cpp
+++ Prac4/src/Prac4.cpp
@@ -2,59 +2,71 @@
 
 using namespace std;
 
-bool playing = true;                        // Set false when paused
-bool stopped = false;                       // Set true to close program
-unsigned char buffer[2][BUFFER_SIZE][2];
-int buffer_location = 0;
-bool bufferReading = 0;                     // Using to switch between column 0 and 1 - the first column
-bool threadReady = false;                   // Using to finish writing the first column at the start of the song, before the column is played
+bool            playing         = true;                     // Set false when paused
+bool            stopped         = false;                    // Set true to close program
+unsigned char   buffer[2][BUFFER_SIZE][2];                  // [bufferReading][bufferLocation][]
+int             bufferLocation  = 0;
+bool            bufferReading   = 0;                        // Using to switch between column 0 and 1 - the first column
+bool            threadReady     = false;                    // Using to finish writing the first column before playback
 
-                                            // Interrupts. Use hardware debouncing
+                                                            // Interrupts. Use hardware debouncing
 void play_pause_isr(void){
-                                            // TODO
+    playing = !playing;
+    digitalWrite(PAUSE_BUTTON, playing);
 }
 
 void stop_isr(void){
-                                            // TODO
+    stopped = true;
+    digitalWrite(STOP_LED, HIGH);
 }
-                                            
+
+bool setup = false;                                            
 int setup_gpio(void){                                               // Setup Function. Called once.
+    if(setup)                                                       // Prevent multiple setups
+        return 0;
+    
     printf("Running init_gpio().\n");
     wiringPiSetup();                                                // Set up WiringPi
                                                                     
-   	for (int j = 0 ; j < sizeof(BTNS) / sizeof(BTNS[0]) ; j++) {    // Set up buttons
+   	for (int j = 0 ; j < sizeof(BTNS) / sizeof(BTNS[0]) ; j++) {    // Set up Buttons
 		pinMode(BTNS[j], INPUT);
-		//pullUpDnControl(BTNS[j], PUD_UP);                         // Disabled for HW Debouncing
+		pullUpDnControl(BTNS[j], PUD_DOWN);                         // PDR for Op-Amp to push
 	}
 
     wiringPiISR(PAUSE_BUTTON,   INT_EDGE_FALLING, &play_pause_isr);
 	wiringPiISR(STOP_BUTTON,    INT_EDGE_FALLING, &stop_isr);
-                                                                    // TODO set up the SPI interface
 
+                                                                    //Set up LEDs
+    for(int i=0; i<(sizeof(LEDS)/sizeof(LEDS[0])); i++)){
+        pinMode(LEDS[i], OUTPUT);
+    }
+                                                                    // TODO set up the SPI interface
+    
+    setup = true;
     return 0;
 }
-
-/* 
- * Thread that handles writing to SPI
- * 
- * You must pause writing to SPI if not playing is true (the player is paused)
- * When calling the function to write to SPI, take note of the last argument.
- * You don't need to use the returned value from the wiring pi SPI function
- * You need to use the buffer_location variable to check when you need to switch buffers
- */
+                                                        /* 
+                                                        * Thread that handles writing to SPI
+                                                        * 
+                                                        * You must pause writing to SPI if not playing is true (the player is paused)
+                                                        * When calling the function to write to SPI, take note of the last argument.
+                                                        * You don't need to use the returned value from the wiring pi SPI function
+                                                        * You need to use the bufferLocation variable to check when you need to switch buffers
+                                                        */
 void *playThread(void *threadargs){
-    while(!threadReady)                     // Wait until tread ready
+    while(!threadReady)                                             // Wait until tread ready
         continue;
     
-    while(!stopped){                        // Only play if not stopped
-		                                    // TODO Suspend playing if paused
-                                            // TODO Write the buffer out to SPI
+    while(!stopped){                                                // Only play if not stopped
+		while(!playing)                                             // Suspend playing if paused
+            delay(50);                                  
+                                                                    // TODO Write the buffer out to SPI
 		
-        //Do some maths to check if you need to toggle buffers
-        buffer_location++;
-        if(buffer_location >= BUFFER_SIZE) {
-            buffer_location = 0;
-            bufferReading = !bufferReading; // switches column one it finishes one column
+                                                                    // Check if toggle buffers needed
+        bufferLocation++;
+        if(bufferLocation >= BUFFER_SIZE) {
+            bufferLocation = 0;
+            bufferReading = !bufferReading;                         // Switches column once it finishes current column
         }
     }
     pthread_exit(NULL);
@@ -74,24 +86,24 @@ int main(){
     sched_param param;
     
     pthread_attr_init (&tattr);
-    pthread_attr_getschedparam (&tattr, &param);                /* safe to get existing scheduling param */
-    param.sched_priority = newprio;                             /* set the priority; others are unchanged */
-    pthread_attr_setschedparam (&tattr, &param);                /* setting the new scheduling param */
-    pthread_create(&thread_id, &tattr, playThread, (void *)1);  /* with new priority specified *
+    pthread_attr_getschedparam (&tattr, &param);                    /* safe to get existing scheduling param */
+    param.sched_priority = newprio;                                 /* set the priority; others are unchanged */
+    pthread_attr_setschedparam (&tattr, &param);                    /* setting the new scheduling param */
+    pthread_create(&thread_id, &tattr, playThread, (void *)1);      /* with new priority specified *
     
-    /*
-     * Read from the file, character by character
-     * You need to perform two operations for each character read from the file
-     * You will require bit shifting
-     * 
-     * buffer[bufferWriting][counter][0] needs to be set with the control bits
-     * as well as the first few bits of audio
-     * 
-     * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
-     * 
-     * Don't forget to check if you have pause set or not when writing to the buffer
-     * 
-     */
+                                                                /*
+                                                                * Read from the file, character by character
+                                                                * You need to perform two operations for each character read from the file
+                                                                * You will require bit shifting
+                                                                * 
+                                                                * buffer[bufferWriting][counter][0] needs to be set with the control bits
+                                                                * as well as the first few bits of audio
+                                                                * 
+                                                                * buffer[bufferWriting][counter][1] needs to be set with the last audio bits
+                                                                * 
+                                                                * Don't forget to check if you have pause set or not when writing to the buffer
+                                                                * 
+                                                                */
                                                                 
     char ch;
     FILE *filePointer;
@@ -108,14 +120,15 @@ int main(){
 
                                                                 
 	 while( (ch = fgetc(filePointer)) != EOF){                                      // Loop to read from the file
-        while( (threadReady && bufferWriting) == (bufferReading && counter) == 0){
+        while( threadReady && bufferWriting == bufferReading && counter == 0){
                                                                                     // Wait until after it has written to a side,
             continue;                                                               // and the thread is still reading from the other side
         }
-                                                                                    //Set config bits for first 8 bit packet and OR with upper bits
+                                                                                    //Set config bits for first 8 bit packet and OR with upper bits (bits 0-7)
         buffer[bufferWriting][counter][0] = ;                                           //TODO
-                                                                                    //Set next 8 bit packet
+                                                                                    //Set next 8 bit packet (bits 8-15)
         buffer[bufferWriting][counter][1] = ;                                           //TODO
+        printf("Saved to Buffer:\t%s\t%s", buffer[bufferWriting][counter][0], buffer[bufferWriting][counter][1])
         counter++;
         if(counter >= BUFFER_SIZE+1){
             if(!threadReady){
@@ -132,5 +145,7 @@ int main(){
                                                                                     
 	pthread_join(thread_id, NULL);                     // Join and exit the playthread
     pthread_exit(NULL);
+
+    digitalWrite(STOP_BUTTON, HIGH);
     return 0;
 }
--- Prac4/src/Prac4.h
+++ Prac4/src/Prac4.h
@@ -1,6 +1,6 @@
 #ifndef PRAC4_H
 #define PRAC4_H
-                                            // Includes
+                                                    // Includes
 #include <wiringPi.h>
 #include <vector>
 #include <stdio.h>
@@ -10,19 +10,23 @@
 #include <math.h>
 #include <pthread.h>
 #include <iostream>
-                                            // Define buttons
+                                                    // Define buttons
 #define PAUSE_BUTTON    18                            
 #define STOP_BUTTON     23
-#define BTNS            [PAUSE_BUTTON, STOP_BUTTON]                    
+#define BTNS            [PAUSE_BUTTON, STOP_BUTTON]
+                                                    //Define LEDS
+#define PAUSE_LED       22
+#define STOP_LED        27
+#define LEDS            [PAUSE_LED, STOP_LED]
 
 #define BUFFER_SIZE     1000
-                                            // SPI Settings
-#define SPI_CHAN    0                           // TODO
-#define SPI_SPEED                               // TODO
+                                                    // SPI Settings
+#define SPI_CHAN        0
+#define SPI_SPEED                                       // TODO
 
-#define FILENAME "src/sound_16k_8bit.raw"   // Sound file name
+#define FILENAME "src/sound_16k_8bit.raw"           // Sound file name
 
-                                            //Function definitions
+                                                    //Function definitions
 void play_audio(void);
 void stop_audio(void);
 int setup_gpio(void);
